9e360414f80a59eb1e86fb4c9434ab33a063d8f1
Milagros Orozco <milaorozco2@gmail.com>
2021-11-24 21:31:24 -0300

    UI y SFX de monedas

--- scenes/EscenasGameplay/Tutorial/Tutoria.gd
+++ scenes/EscenasGameplay/Tutorial/Tutoria.gd
@@ -2,18 +2,20 @@ extends Spatial
 
 
 var vidas = 3
-
+onready var hp = $UI/corazones
 onready var anim = $Player/animacion
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-
-
 func _on_Limite_body_entered(_body):
 	anim.play("reset")
 	vidas -= 1
+	if vidas == 2:
+		hp.play("dosvidastransicion")
+	if vidas == 1:
+		hp.play("unavidatransicion")
 	if vidas <= 0:
 		get_tree().change_scene("res://scenes/game_over/game_over.tscn")
 	pass # Replace with function body.
@@ -22,3 +24,4 @@ func _on_Limite_body_entered(_body):
 func _on_Final_body_entered(_body):
 	get_tree().change_scene("res://scenes/EscenasGameplay/Nivel 2/level_2.tscn")
 	pass # Replace with function body.
+
