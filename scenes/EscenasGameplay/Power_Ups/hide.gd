2e96a1232266191772f1c83e62c95c9f11766cff
Milagros Orozco <milaorozco2@gmail.com>
2021-11-26 22:09:12 -0300

    Revert "Merge branch 'temporizador' into main"
    This reverts commit 54d0b19db77f3539f4045ae781b1398d50794c50.

--- scenes/EscenasGameplay/Power_Ups/hide.gd
+++ scenes/EscenasGameplay/Power_Ups/hide.gd
@@ -1,8 +1,6 @@
 extends Area
 onready var anim = $anim
-signal temporizador()
 
 func _on_hide_body_entered(_body):
 	$anim.play ("hide")
-	emit_signal("temporizador")
 	pass # Replace with function body.
