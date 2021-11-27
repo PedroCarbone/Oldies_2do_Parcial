628a1c433f3822bfc3746d99d35a4eed68a27407
Milagros Orozco <milaorozco2@gmail.com>
2021-11-26 21:18:22 -0300

    temporizador

--- scenes/EscenasGameplay/Power_Ups/hide.gd
+++ scenes/EscenasGameplay/Power_Ups/hide.gd
@@ -1,6 +1,8 @@
 extends Area
 onready var anim = $anim
+signal temporizador()
 
 func _on_hide_body_entered(_body):
 	$anim.play ("hide")
+	emit_signal("temporizador")
 	pass # Replace with function body.
